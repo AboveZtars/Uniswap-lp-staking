8491b51974cbcc4084b5fe7d3348c10809472cd1
Rafael Molina <rafael@molina-aquino.com>
2021-10-03 08:52:33 -0400

    add deploy scripts

--- deploy/UniswapLPStakingDeploy.js
+++ deploy/UniswapLPStakingDeploy.js
@@ -2,11 +2,6 @@
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
-    //deploying reward token contract
-    const ArepaToken = await deploy('ArepaToken', {
-      from:deployer,
-      log:true,
-    })
     //deploying staking contract
     await deploy('UniswapLPStaking', {
       from: deployer,
@@ -22,11 +17,5 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     let stakeInit = await deployments.get("UniswapLPStaking")
     stakeInit = await ethers.getContractAt("UniswapLPStaking", stakeInit.address)
     await stakeInit.initialize(ArepaToken.address, deployer, '100000000000000000000', '13224139', '13324139')
-    //deploying swapping contract
-    await deploy('ToolV2', {
-      from: deployer,
-      log:true,
-    })
-
   };
   module.exports.tags = ['UniswapLPStaking'];
